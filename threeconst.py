23f19de0cd8b664d9068d2368323e4fa0aa3b00a
Hampuse1 <hampuse1@hotmail.com>
2021-09-13 17:08:25 +0200

    Updates

--- threeconst.py
+++ threeconst.py
@@ -1,10 +1,47 @@
 import pygame
 
 WIDTH, HEIGHT = 450, 450
-WHITE = (255, 222, 255)
-BLACK = (0,0,0)
-RED = (255,0,0)
-YELLOW = (255,255,0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 FPS = 60
 BOARD_SIZE = 3
+PLAYERMARKS = ["X", "O"]
+
+
+class Button():
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+
+    def draw(self, win, outline=None):
+        # Call this method to draw the button on the screen
+        if outline:
+            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 60)
+            text = font.render(self.text, 1, (0, 0, 0))
+            win.blit(text,
+                     (self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+
+
+    def isOver(self, pos, text):
+
+        # Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                if self.text == "":
+                    self.text = text
+                return True
+
+        return False
 
--- threeinrow.py
+++ threeinrow.py
@@ -2,25 +2,57 @@ import pygame
 import os
 from threeconst import *
 import numpy as np
+import time
 
 pygame.font.init()
+pygame.init()
 
 # Skapar ett fönster
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Three in row")
 
 
-def draw_window(board):
+def draw_window(board, buttons):
+    for button in buttons:
+        button.draw(WIN)
 
     pygame.display.update()
 
+def checkwin(buttons):
+    if buttons[0].text == buttons[1].text and buttons[0].text == buttons[2].text:
+        pygame.quit()
+
+    elif buttons[0].text == buttons[3].text and buttons[0].text == buttons[6].text:
+        pygame.quit()
+
+    elif buttons[6].text == buttons[7].text and buttons[6].text == buttons[8].text:
+        pygame.quit()
+
+    elif buttons[2].text == buttons[5].text and buttons[2].text == buttons[8].text:
+        pygame.quit()
+
+    elif buttons[0].text == buttons[4].text and buttons[0].text == buttons[8].text:
+        pygame.quit()
+
+    elif buttons[6].text == buttons[4].text and buttons[6].text == buttons[2].text:
+        pygame.quit()
+
 
 def main():
-    board = []
+    buttons = []
     clock = pygame.time.Clock()
     run = True
     board = np.zeros((BOARD_SIZE, BOARD_SIZE))
     print(board)
+    turn = 0
+
+    for x in range(3):
+        for y in range(3):
+            print("Adding button")
+
+            buttons.append(Button(WHITE, x*150, y*150, 145, 145))
+
+
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -28,7 +60,14 @@ def main():
                 run = False
                 pygame.quit()
 
-        draw_window(board)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+
+                for button in buttons:
+                    if button.isOver(pygame.mouse.get_pos(),PLAYERMARKS[turn % 2]):
+                        turn += 1
+                        #checkwin(buttons)
+
+        draw_window(board, buttons)
 
 
     main()
