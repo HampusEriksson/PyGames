5d9abd7092c1e0ceb0026c198f25f0f73629c0e4
Hampuse1 <hampuse1@hotmail.com>
2021-09-21 09:22:06 +0200

    Kinda playable game

--- Snake/snake.py
+++ Snake/snake.py
@@ -13,32 +13,43 @@ pygame.display.set_caption("Snake")
 
 def draw_window(win, all_sprites):
     win.fill(0)
-
     all_sprites.draw(win)
-
     pygame.display.update()
 
 
 
 def main():
+    score = 0
     clock = pygame.time.Clock()
     all_sprites = pygame.sprite.Group()
 
     apple = Apple()
     all_sprites.add(apple)
-
     game_over = False
     head = Snakepart()
     all_sprites.add(head)
     apple.move()
+    body = []
 
     while not game_over:
         clock.tick(FPS)
-        head.update()
+
 
         if head.collide(apple):
             apple.move()
+            part = Snakepart()
+            all_sprites.add(part)
+            body.append(part)
+            head.speed *= 1.02
+            score +=1
 
+        newpos = [head.rect.center] + [x.rect.center for x in body]
+
+        if len(body) >= 1:
+            for i in range(len(body)):
+                body[i].rect.center = newpos[i]
+
+        head.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -46,13 +57,13 @@ def main():
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
-                    head.direction = (0,-1)
+                    head.direction = (0,-1) if head.direction != (0,1) else head.direction
                 if event.key == pygame.K_a:
-                    head.direction = (-1,0)
+                    head.direction = (-1,0) if head.direction != (1,0) else head.direction
                 if event.key == pygame.K_s:
-                    head.direction = (0,1)
+                    head.direction = (0,1) if head.direction != (0,-1) else head.direction
                 if event.key == pygame.K_d:
-                    head.direction = (1,0)
+                    head.direction = (1,0) if head.direction != (-1,0) else head.direction
 
 
 
--- Snake/snakeconst.py
+++ Snake/snakeconst.py
@@ -8,22 +8,35 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 GREEN = (40,100,20)
-UNITSIZE = 50
+APPLESIZE = 50
+UNITSIZE = 30
+
 FPS = 60
-SPEED = 5
 
 class Snakepart(pygame.sprite.Sprite):
-    def __init__(self, color=GREEN):
+    def __init__(self, center = (WIDTH//2, HEIGHT//2), color=GREEN):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((UNITSIZE, UNITSIZE))
         self.image.fill(color)
         self.rect = self.image.get_rect()
-        self.rect.center = (UNITSIZE // 2, UNITSIZE // 2)
+        self.rect.center = (center)
         self.direction = (0, 0)
+        self.speed = 5
 
     def update(self):
-        self.rect.x += self.direction[0]*SPEED
-        self.rect.y += self.direction[1]*SPEED
+        self.rect.x += self.direction[0]*self.speed
+        self.rect.y += self.direction[1]*self.speed
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+        if self.rect.right<0:
+            self.rect.left = WIDTH
+
+        if self.rect.bottom<0:
+            self.rect.top = HEIGHT
+
+        if self.rect.top > HEIGHT:
+            self.rect.bottom = 0
 
     def collide(self, spritegroup):
         if type(spritegroup) != list:
@@ -39,10 +52,10 @@ class Apple(pygame.sprite.Sprite):
     def __init__(self, x=WIDTH//2, y=HEIGHT//2, color=RED):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface((UNITSIZE, UNITSIZE))
+        self.image = pygame.Surface((APPLESIZE, APPLESIZE))
         self.image.fill(color)
         self.rect = self.image.get_rect()
-        self.rect.center = (UNITSIZE // 2, UNITSIZE // 2)
+        self.rect.center = (APPLESIZE // 2, APPLESIZE // 2)
 
     def move(self):
         print("moving")
