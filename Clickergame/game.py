9a3ce3f0a72e4bd3dc9450a2b87a6321c73fa4dc
Hampuse1 <hampuse1@hotmail.com>
2021-10-11 13:24:16 +0200

    Updates from pycharm

--- Clickergame/game.py
+++ Clickergame/game.py
@@ -8,10 +8,41 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Clickergame")
 
 
-def draw(buttons):
-
+def draw(win, buttons, plantedtrees):
+    x = plantedtrees
+    win.fill(0)
     for button in buttons:
-        button.draw()
+        button.draw(win)
+    while True:
+        if x - 10 > 0:
+            x -= 10
+
+            win.blit(
+                pygame.transform.scale(
+                    pygame.image.load("Assets/eucalyptus.png"),
+                    (WIDTH // 5, HEIGHT // 12),
+                ),
+                (
+                    random.randrange(WIDTH // 4, (WIDTH * 3) // 4),
+                    random.randrange(HEIGHT // 4, (HEIGHT * 3) // 4),
+                ),
+            )
+
+        if x - 1 >= 0:
+            x -= 1
+            win.blit(
+                pygame.transform.scale(
+                    pygame.image.load("Assets/tree.png"),
+                    (WIDTH // 5, HEIGHT // 12),
+                ),
+                (
+                    random.randrange(WIDTH // 4, (WIDTH * 3) // 4),
+                    random.randrange(HEIGHT // 4, (HEIGHT * 3) // 4),
+                ),
+            )
+
+        if x <= 0:
+            break
 
     pygame.display.update()
 
@@ -19,13 +50,17 @@ def draw(buttons):
 def main():
     clock = pygame.time.Clock()
     buttons = []
-    for button, i in enumerate(BUTTONNAMES):
-        buttons.append(Button(RED, (i+1)*(HEIGHT // 12), i*10, i*100, button))
+    plantedtrees = 0
+    trees = []
+    for i, button in enumerate(BUTTONNAMES):
+        buttons.append(Button(button, (i + 1) * (HEIGHT // 12), i * 10, i * 100))
 
-
+    counter = 0
 
     while True:
         clock.tick(FPS)
+        if counter % FPS == 0:
+            plantedtrees += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -34,12 +69,12 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for button in buttons:
                     if button.isClicked(pygame.mouse.get_pos()):
-                        pass
+                        print("Clicked")
 
         for button in buttons:
-                button.isOver(pygame.mouse.get_pos())
+            button.isOver(pygame.mouse.get_pos())
 
-        draw(buttons)
+        draw(WIN, buttons, plantedtrees)
 
 
 if __name__ == "__main__":
